a901d0c432c210521b5fdef11391cf5792212738
MikeFranco <mfranco_98@yahoo.com>
2021-12-11 23:36:48 -0600

    feat: added homepage main header

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import SignUp from "./pages/SignUp";
-import Login from "./pages/Login";
-import NoPage from "./pages/NoPage";
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import NoPage from './pages/NoPage';
+import "antd/dist/antd.css";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="sign-up" element={<SignUp />} />
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<NoPage />} />
+          <Route path='sign-up' element={<SignUp />} />
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,29 @@
+import { Row, Col } from 'antd';
+import '../index.css';
+import principal from '../assets/img/principal.jpg';
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div
+      clasName='nuestra-boda'
+      style={{
+        height: '100vh',
+        width: '100vw',
+        backgroundImage: `url(${principal})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+        height: '100vh'
+      }}>
+      <Row justify='center'>
+        <Col span={5}>
+          {' '}
+          <h2 className='text-white'>Nuestra Boda</h2>
+          <h2 className='text-importante'>Diana &amp; Steve</h2>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Home;
